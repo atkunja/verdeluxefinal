b2d5f3fc9c0472be13b4d97f503ba7ceb6f137b5
Ayush Kunjadia <atkunjadia@gmail.com>
2025-12-24 19:02:22 -0500

    Fix: Prevent crash when STRIPE_SECRET_KEY is missing

--- src/server/stripe/client.ts
+++ src/server/stripe/client.ts
@@ -1,6 +1,8 @@
 import Stripe from "stripe";
 import { env } from "../env";
 
-export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: "2022-11-15",
-});
+export const stripe = env.STRIPE_SECRET_KEY
+  ? new Stripe(env.STRIPE_SECRET_KEY, {
+    apiVersion: "2022-11-15",
+  })
+  : (null as unknown as Stripe);
