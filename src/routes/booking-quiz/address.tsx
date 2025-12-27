bf5934fd850b974e7d92d183d234a620728dd217
Ayush Kunjadia <atkunjadia@gmail.com>
2025-12-27 16:49:57 -0500

    changes

--- src/routes/booking-quiz/address.tsx
+++ src/routes/booking-quiz/address.tsx
@@ -41,20 +41,52 @@ function MapPreview({ lat, lng }: { lat?: number; lng?: number }) {
         disableDefaultUI: true,
         styles: [
           {
-            featureType: "all",
-            elementType: "geometry",
-            stylers: [{ color: "#f5f5f5" }],
-          },
-          {
-            featureType: "all",
-            elementType: "labels.text.fill",
-            stylers: [{ color: "#616161" }],
-          },
-          {
             featureType: "water",
             elementType: "geometry",
-            stylers: [{ color: "#e9e9e9" }]
-          }
+            stylers: [{ color: "#e9e9e9" }, { lightness: 17 }],
+          },
+          {
+            featureType: "landscape",
+            elementType: "geometry",
+            stylers: [{ color: "#f5f5f5" }, { lightness: 20 }],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "geometry.fill",
+            stylers: [{ color: "#ffffff" }, { lightness: 17 }],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "geometry.stroke",
+            stylers: [{ color: "#ffffff" }, { lightness: 29 }, { weight: 0.2 }],
+          },
+          {
+            featureType: "road.arterial",
+            elementType: "geometry",
+            stylers: [{ color: "#ffffff" }, { lightness: 18 }],
+          },
+          {
+            featureType: "road.local",
+            elementType: "geometry",
+            stylers: [{ color: "#ffffff" }, { lightness: 16 }],
+          },
+          {
+            featureType: "poi",
+            elementType: "geometry",
+            stylers: [{ color: "#f5f5f5" }, { lightness: 21 }],
+          },
+          {
+            elementType: "labels.text.stroke",
+            stylers: [{ visibility: "on" }, { color: "#ffffff" }, { lightness: 16 }],
+          },
+          {
+            elementType: "labels.text.fill",
+            stylers: [{ saturation: 36 }, { color: "#333333" }, { lightness: 40 }],
+          },
+          {
+            elementType: "labels.icon",
+            stylers: [{ visibility: "off" }],
+          },
         ],
       });
       new window.google.maps.Marker({
